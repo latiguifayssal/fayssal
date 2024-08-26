5b5cd938622edb9b4816697cbba146ea1e96960a
latiguifayssal <fayssallatigui0@gmail.com>
2024-08-26 13:22:01 +0100

    first commit

---
+++ src/main/java/com/example/fayssal/FayssalApplication.java
@@ -0,0 +1,12 @@
+package com.example.fayssal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FayssalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FayssalApplication.class, args);
+    }
+}
